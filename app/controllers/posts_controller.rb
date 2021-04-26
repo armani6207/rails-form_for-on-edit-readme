fef90585e9b14e67529710ef6d45fe98bb21a164
Armando <rojas.armani@gmail.com>
2021-04-26 15:40:15 -0400

    commit tree ad99ff29106a6087ccf37c85ad66f86f991831a4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
+	  @post = Post.new(post_params)
 	  @post.save
 	  redirect_to post_path(@post)
 	end
@@ -24,8 +22,15 @@ class PostsController < ApplicationController
 	end
 
 	def update
+		params
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :description)
 	end
 end
